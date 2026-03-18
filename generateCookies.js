31d4412e49d2c8c668065d1a90dcfec4fbc34bee
giowjf <gjfranchi@gmail.com>
2026-03-17 22:02:53 -0300

    Update generateCookies.js with login/password

--- generateCookies.js
+++ generateCookies.js
@@ -14,8 +14,28 @@ const browser = await puppeteer.launch({
 
 const page = await browser.newPage();
 
+console.log("Abrindo login Google...");
+
+await page.goto("https://accounts.google.com/signin/v2/identifier", {
+  waitUntil: "networkidle2"
+});
+
+await page.type('input[type="email"]', process.env.YT_EMAIL);
+
+await page.click("#identifierNext");
+
+await page.waitForTimeout(3000);
+
+await page.type('input[type="password"]', process.env.YT_PASSWORD);
+
+await page.click("#passwordNext");
+
+await page.waitForTimeout(8000);
+
+console.log("Entrando no YouTube...");
+
 await page.goto("https://www.youtube.com", {
-waitUntil: "networkidle2"
+  waitUntil: "networkidle2"
 });
 
 const cookies = await page.cookies();
@@ -23,20 +43,20 @@ const cookies = await page.cookies();
 let cookieTxt = "";
 
 cookies.forEach(c => {
-cookieTxt += [
-".youtube.com",
-"TRUE",
-c.path,
-c.secure ? "TRUE" : "FALSE",
-c.expires || "0",
-c.name,
-c.value
-].join("\t") + "\n";
+  cookieTxt += [
+    ".youtube.com",
+    "TRUE",
+    c.path,
+    c.secure ? "TRUE" : "FALSE",
+    c.expires || "0",
+    c.name,
+    c.value
+  ].join("\t") + "\n";
 });
 
 fs.writeFileSync("cookies.txt", cookieTxt);
 
-console.log("cookies.txt gerado");
+console.log("cookies.txt gerado com conta logada");
 
 await browser.close();
 
