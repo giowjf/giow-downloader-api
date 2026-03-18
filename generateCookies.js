bb553e6bc32c9c9b262ca442f99220cd1c40e04a
giowjf <gjfranchi@gmail.com>
2026-03-17 21:58:53 -0300

    Update generateCookies.js add browser

--- generateCookies.js
+++ generateCookies.js
@@ -4,8 +4,12 @@ const fs = require("fs");
 (async () => {
 
 const browser = await puppeteer.launch({
-headless: true,
-args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  headless: true,
+  executablePath: "/usr/bin/chromium",
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox"
+  ]
 });
 
 const page = await browser.newPage();
