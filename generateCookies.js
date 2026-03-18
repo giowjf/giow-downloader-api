31f2091214ce360a8c3162a6ed27091a8a9f840b
giowjf <gjfranchi@gmail.com>
2026-03-17 21:51:36 -0300

    Create generateCookies.js

---
+++ generateCookies.js
@@ -0,0 +1,39 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+
+const browser = await puppeteer.launch({
+headless: true,
+args: ["--no-sandbox", "--disable-setuid-sandbox"]
+});
+
+const page = await browser.newPage();
+
+await page.goto("https://www.youtube.com", {
+waitUntil: "networkidle2"
+});
+
+const cookies = await page.cookies();
+
+let cookieTxt = "";
+
+cookies.forEach(c => {
+cookieTxt += [
+".youtube.com",
+"TRUE",
+c.path,
+c.secure ? "TRUE" : "FALSE",
+c.expires || "0",
+c.name,
+c.value
+].join("\t") + "\n";
+});
+
+fs.writeFileSync("cookies.txt", cookieTxt);
+
+console.log("cookies.txt gerado");
+
+await browser.close();
+
+})();
